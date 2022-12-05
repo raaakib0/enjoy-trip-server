cc78212904f9a152f069aeb087ed77ba1340f8c9
md-rakibul-islam <raaakib0@gmail.com>
2022-12-06 01:21:26 +0600

    add services

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.98xvyli.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function verifyJWT(req, res, next){
+function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
 
-    if(!authHeader){
-        return res.status(401).send({message: 'unauthorized access'});
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
     }
     const token = authHeader.split(' ')[1];
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
-        if(err){
-            return res.status(403).send({message: 'Forbidden access'});
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
         }
         req.decoded = decoded;
         next();
@@ -35,18 +35,36 @@ function verifyJWT(req, res, next){
 
 async function run() {
     try {
-        const serviceCollection = client.db('geniusCar').collection('services');
-        const orderCollection = client.db('geniusCar').collection('orders');
+        const serviceCollection = client.db('enjoyTrip').collection('services');
+        const orderCollection = client.db('enjoyTrip').collection('orders');
 
-        app.post('/jwt', (req, res) =>{
+        app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d'})
-            res.send({token})
-        })  
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '60d' })
+            res.send({ token })
+        })
 
         app.get('/services', async (req, res) => {
-            const query = {}
-            const cursor = serviceCollection.find(query);
+            const search = req.query.search
+            console.log(search);
+            let query = {};
+            if (search.length) {
+                query = {
+                    $text: {
+                        $search: search
+                    }
+                }
+
+            }
+            // const query = { price: { $gt: 100, $lt: 300 } }
+            // const query = { price: { $eq: 200 } }
+            // const query = { price: { $lte: 200 } }
+            // const query = { price: { $ne: 150 } }
+            // const query = { price: { $in: [20, 40, 150] } }
+            // const query = { price: { $nin: [20, 40, 150] } }
+            // const query = { $and: [{price: {$gt: 20}}, {price: {$gt: 100}}] }
+            const order = req.query.order === 'asc' ? 1 : -1;
+            const cursor = serviceCollection.find(query).sort({ price: order });
             const services = await cursor.toArray();
             res.send(services);
         });
@@ -62,9 +80,9 @@ async function run() {
         // orders api
         app.get('/orders', verifyJWT, async (req, res) => {
             const decoded = req.decoded;
-            
-            if(decoded.email !== req.query.email){
-                res.status(403).send({message: 'unauthorized access'})
+
+            if (decoded.email !== req.query.email) {
+                res.status(403).send({ message: 'unauthorized access' })
             }
 
             let query = {};
@@ -89,7 +107,7 @@ async function run() {
             const status = req.body.status
             const query = { _id: ObjectId(id) }
             const updatedDoc = {
-                $set:{
+                $set: {
                     status: status
                 }
             }
@@ -116,9 +134,9 @@ run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
-    res.send('genius car server is running')
+    res.send('enjoyTrip server is running')
 })
 
 app.listen(port, () => {
-    console.log(`Genius Car server running on ${port}`);
+    console.log(`EnjoyTrip server running on ${port}`);
 })
